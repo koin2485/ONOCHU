36ce34049606fcf671c940800880f17e7549e7d0
koin2485 <koin2485@gmail.com>
2025-03-26 21:42:03 +0900

    전체적인업데이트 ㅎㅎ

--- src/components/SongCard.jsx
+++ src/components/SongCard.jsx
@@ -6,12 +6,13 @@ const SongCard = ({ data, onReset }) => {
       <div className="lyrics-section">
         <p className="lyrics">{data.lyrics}</p>
         {data.translation && <p className="translation">{data.translation}</p>}
+        <p className="duration">{data.duration}</p>
+
       </div>
       <hr />
       <div className="info-section">
         <p className="title">{data.title}</p>
         <p className="artist">{data.artist}</p>
-        <p className="duration">{data.duration}</p>
       </div>
     </div>
   );
